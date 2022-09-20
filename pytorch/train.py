d78d807d17f960b30362cf5569773caeb4c21178
kangame2000 <kangame2000@gmail.com>
2022-09-20 10:23:55 +0800

    add time test

--- pytorch/train.py
+++ pytorch/train.py
@@ -13,7 +13,7 @@ from torchvision import datasets, transforms    # torchvision contains common ut
 from torch.autograd import Variable
 
 from .model import Net
-
+from datetime import datetime
 
 def load_data(train_batch_size, test_batch_size):
     """Fetch MNIST dataset
@@ -25,7 +25,7 @@ def load_data(train_batch_size, test_batch_size):
 
     # Fetch training data
     train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('../data', train=True, download=True,
+        datasets.MNIST('./data', train=True, download=True,
                        transform=transforms.Compose([
                            transforms.ToTensor(),
                            transforms.Normalize((0.1307,), (0.3081,))
@@ -34,7 +34,7 @@ def load_data(train_batch_size, test_batch_size):
 
     # Fetch test data
     test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('../data', train=False, transform=transforms.Compose([
+        datasets.MNIST('./data', train=False, transform=transforms.Compose([
                            transforms.ToTensor(),
                            transforms.Normalize((0.1307,), (0.3081,))
                        ])),
@@ -103,7 +103,14 @@ def test(model, test_loader):
         100. * correct / len(test_loader.dataset)))
 
 
+def gettime():
+    now = datetime.now()
+    time_str = now.strftime("%H:%M:%S")
+    return time_str
+
+
 if __name__ == '__main__':
+    start_time = gettime()
     # Set up training settings from command line options, or use default
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
     parser.add_argument('--batch-size', type=int, default=64, metavar='N',
@@ -137,11 +144,30 @@ if __name__ == '__main__':
 
     # Train & test the model
     for epoch in range(1, args.epochs + 1):
+        time1 = gettime()
         train(model, optimizer, epoch, train_loader, log_interval=args.log_interval)
         test(model, test_loader)
+        time2= gettime()
 
+        time_1 = datetime.strptime(time1,"%H:%M:%S")
+        time_2 = datetime.strptime(time2,"%H:%M:%S")
+        time_interval = time_2 - time_1
+
+        print("epoch" , epoch , "strat:", time1)
+        print("epoch" , epoch , "  end:", time2)
+        print("epoch" , epoch , "  耗時:", time_interval)
+        print("------------------------------")
 
     # Save the model for future use
     package_dir = os.path.dirname(os.path.abspath(__file__))
     model_path = os.path.join(package_dir,'model')
     torch.save(model.state_dict(), model_path)
+
+    end_time= gettime()
+    time_1 = datetime.strptime(start_time,"%H:%M:%S")
+    time_2 = datetime.strptime(end_time,"%H:%M:%S")
+    time_interval = time_2 - time_1
+    print("------------------------------")
+    print("start_Time is :", start_time)
+    print("  end_Time is :", end_time)
+    print("         總耗時:", time_interval)
